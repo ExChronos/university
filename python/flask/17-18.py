2c5b1934c5191ae847b57c6ac1ebef5fe762a315
Nathan <mega.nemirovskiy@gmail.com>
2024-12-07 11:32:16 +0300

    Доделал задание 17-18

--- python/flask/17-18.py
+++ python/flask/17-18.py
@@ -2,9 +2,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def main():
+    return 'Вы находитесь на главной странице'
+
 @app.route('/square/<int:n>')
 def square(n):
     return f'Квадрат числа {n} равен {n**2}'
+
+@app.route('/length/<string:stroka>')
+def length(stroka):
+    return f'Длина строки `{stroka}` = {len(stroka)}'
+
+@app.route('/add/<int:n1>/<int:n2>')
+def sum(n1, n2):
+    return f'Сумма двух чисел: {n1}, {n2} = {n1+n2}'
+
+@app.route('/error')
+def error():
+    return f'{1/0}'
 
 if __name__ == '__main__':
     app.run(debug=True)
